f6b0d32a87e3805a56c8190a1569808e2d1f3a22
Nigam <nigam21nir@gmail.com>
2023-09-30 00:27:22 +0545

    User Controller and User routes

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,13 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const errorMiddleware = require("./middleware/error");
+const user = require("./routes/userRoutes");
 app.use(express.json());
 app.use(cookieParser());
+
+app.use("/user", user);
+app.use(errorMiddleware);
 
 //Route Imports
 
