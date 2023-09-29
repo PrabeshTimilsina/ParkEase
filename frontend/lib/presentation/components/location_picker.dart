112ff424da0565230bb41bc563126e1628c93319
earthPerson-001 <85444310+earthPerson-001@users.noreply.github.com>
2023-09-29 17:24:44 +0545

    Add location picker for picking a precise location.

---
+++ frontend/lib/presentation/components/location_picker.dart
@@ -0,0 +1,61 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+import 'package:park_ease/presentation/pages/home.dart';
+
+class LocationPicker extends StatefulWidget {
+  const LocationPicker({super.key, this.initialLocation, this.initialAddress});
+
+  final String? initialAddress;
+  final GeoPoint? initialLocation;
+
+  @override
+  State<LocationPicker> createState() => _LocationPickerState();
+}
+
+class _LocationPickerState extends State<LocationPicker> {
+ 
+  late PickerMapController pickerController = PickerMapController(
+      initMapWithUserPosition:
+          const UserTrackingOption(enableTracking: true, unFollowUser: true));
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPickerLocation(
+        controller: pickerController,
+        onMapReady: (ready) {
+          if (ready) {
+            pickerController.osmBaseController.setZoom(zoomLevel: 18);
+            if (widget.initialLocation != null) {
+              // setting to current location
+              pickerController.osmBaseController
+                  .goToPosition(widget.initialLocation!);
+            }
+          }
+        },
+        bottomWidgetPicker: Positioned(
+            bottom: 12,
+            right: 8,
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    iconSize: 30,
+                    icon: const Icon(Icons.location_searching_sharp),
+                    onPressed: () {
+                      pickerController.osmBaseController.currentLocation();
+                    },
+                  ),
+                  IconButton(
+                    iconSize: 30,
+                    icon: const Icon(Icons.arrow_circle_right_outlined),
+                    onPressed: () async {
+                      GeoPoint p =
+                          await pickerController.selectAdvancedPositionPicker();
+                      Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => const MyHomePage(title: "Home",)));
+                    },
+                  )
+                ])));
+  }
+}
