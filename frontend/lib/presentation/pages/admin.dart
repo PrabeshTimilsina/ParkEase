4b74ad3b833c271521b127093605fb0f27a6d8be
earthPerson-001 <85444310+earthPerson-001@users.noreply.github.com>
2023-10-01 07:22:18 +0545

    Fix serialization and response checking.

--- frontend/lib/presentation/pages/admin.dart
+++ frontend/lib/presentation/pages/admin.dart
@@ -6,6 +6,8 @@ import 'package:park_ease/data/constants.dart';
 import 'package:park_ease/presentation/components/my_button.dart';
 import 'package:park_ease/presentation/pages/home.dart';
 import 'package:http/http.dart' as http;
+import 'package:park_ease/providers/current_location_model.dart';
+import 'package:provider/provider.dart';
 import '../components/my_textfield.dart';
 
 import 'dart:developer' as developer;
@@ -21,42 +23,48 @@ class _AdminState extends State<Admin> {
   final locationController = TextEditingController();
 
   final typeController = TextEditingController();
-
+  final carRateController = TextEditingController();
+  final motorCycleRateController = TextEditingController();
   final nameController = TextEditingController();
-  final vehicleController = TextEditingController();
-  final capacityController = TextEditingController();
+  final carCapacityController = TextEditingController();
+  final motorCycleCapacityController = TextEditingController();
   double rating = Random().nextDouble();
 
-  void registerPark() async {
+  void registerPark(double latitude, double longitude) async {
     if (nameController.text.isNotEmpty &&
         locationController.text.isNotEmpty &&
-        vehicleController.text.isNotEmpty &&
         typeController.text.isNotEmpty &&
-        capacityController.text.isNotEmpty) {
+        (((carCapacityController.text.isNotEmpty &&
+                carRateController.text.isNotEmpty) ||
+            (motorCycleCapacityController.text.isNotEmpty &&
+                motorCycleRateController.text.isNotEmpty)))) {
       var regBody = {
-        "name": nameController,
+        "name": nameController.text,
         "location": {
-          {
-            "address": locationController,
-            "latitude": "something",
-            "longitude": "something"
-          }
+          "address": locationController.text,
+          "latitude": latitude,
+          "longitude": longitude
         },
         "categories": [
           {
-            "vehicleType": vehicleController,
-            "rate": rating,
-            "capacity": capacityController
+            "vehicleType": "Car",
+            "rate": int.parse(carRateController.text),
+            "capacity": int.parse(carCapacityController.text)
+          },
+          {
+            "vehicleType": "Motorcycle",
+            "rate": int.parse(motorCycleRateController.text),
+            "capacity": int.parse(motorCycleCapacityController.text)
           }
         ],
-        "parkingType": typeController,
+        "parkingType": typeController.text,
       };
       var response = await http.post(Uri.parse(registerlocation),
           headers: {"Content-Type": "application/json"},
           body: jsonEncode(regBody));
       var jsonResponse = jsonDecode(response.body);
-      developer.log(jsonResponse['sucess']);
-      if (jsonResponse['sucess']) {
+      developer.log(jsonResponse.toString());
+      if (response.statusCode == 200) {
         Navigator.push(
             context,
             MaterialPageRoute(
@@ -78,7 +86,7 @@ class _AdminState extends State<Admin> {
               children: [
                 SizedBox(height: mediaquery.height * 0.05),
                 const Text(
-                  "Register your Park",
+                  "Register your Parking Area",
                   textAlign: TextAlign.center,
                   textScaleFactor: 2.0,
                 ),
@@ -103,32 +111,56 @@ class _AdminState extends State<Admin> {
                 ),
                 SizedBox(height: mediaquery.height * 0.025),
                 MyTextField(
-                  controller: vehicleController,
-                  hintText: 'vehicle type',
+                  controller: typeController,
+                  hintText: 'parking type (regulated, unregulated)',
                   obscureText: false,
                 ),
                 SizedBox(height: mediaquery.height * 0.025),
                 MyTextField(
-                  controller: typeController,
-                  hintText: 'parking type',
-                  obscureText: true,
+                  controller: carRateController,
+                  hintText: 'rate per hour for car',
+                  obscureText: false,
                 ),
                 SizedBox(height: mediaquery.height * 0.025),
                 MyTextField(
-                  controller: capacityController,
-                  hintText: 'Capacity',
-                  obscureText: true,
+                  controller: carCapacityController,
+                  hintText: 'Car Capacity',
+                  obscureText: false,
+                ),
+                SizedBox(height: mediaquery.height * 0.025),
+                MyTextField(
+                  controller: motorCycleRateController,
+                  hintText: 'rate per hour for motorcycle',
+                  obscureText: false,
+                ),
+                SizedBox(height: mediaquery.height * 0.025),
+                MyTextField(
+                  controller: motorCycleCapacityController,
+                  hintText: 'Motorcycle Capacity',
+                  obscureText: false,
                 ),
                 SizedBox(height: mediaquery.height * 0.1),
-                MyButton(
-                  buttonName: 'Submit',
-                  onTap: () {
-                    // registerPark();
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                const MyHomePage(title: "Home")));
+                Consumer<CurrentLocationModel>(
+                  builder: (context, currentLocationModel, child) {
+                    return MyButton(
+                      buttonName: 'Submit',
+                      onTap: () {
+                        if (currentLocationModel.currentLocation != null) {
+                          registerPark(
+                              currentLocationModel.currentLocation!.latitude,
+                              currentLocationModel.currentLocation!.longitude);
+                        } else {
+                          registerPark(0, 0);
+                        }
+
+                        // registerPark();
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    const MyHomePage(title: "Home")));
+                      },
+                    );
                   },
                 ),
                 SizedBox(height: mediaquery.height * 0.025),
